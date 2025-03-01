ceea6f9fa5a0a0cfc03fedc6d6ad4aff1e326530
sofimq167 <sofimoreno2015@gmail.com>
2025-03-01 15:57:17 -0500

    Cambios en los fields de fecha

--- task_management/task_app/models.py
+++ task_management/task_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -16,9 +17,9 @@ class Task(models.Model):
     descripcion = models.TextField(blank=True, null=True)
     prioridad = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
     estado = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pendiente')
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_limite = models.DateField(null=True, blank=True)
     completado = models.BooleanField(default=False)
-    
+
 class Meta:
     db_table = 'tasks'
