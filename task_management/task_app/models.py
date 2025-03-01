7528437499b50ea32059b5ac7264c7adcf80672c
sofimq167 <sofimoreno2015@gmail.com>
2025-02-28 22:30:03 -0500

    Agregar los modelos en Models

--- task_management/task_app/models.py
+++ task_management/task_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Baja'),
+        (2, 'Media'),
+        (3, 'Alta'),
+    ]
+    STATUS_CHOICES = [
+        ('Pendiente', 'Pendiente'),
+        ('En progreso', 'En progreso'),
+        ('Completada', 'Completada'),
+    ]
+
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    prioridad = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    estado = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pendiente')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_limite = models.DateField(null=True, blank=True)
+    completado = models.BooleanField(default=False)
+
+class Meta:
+    db_table = 'tasks'
