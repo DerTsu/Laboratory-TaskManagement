33772ae2360a1abcae3b75764f966b671998cc50
sofimq167 <sofimoreno2015@gmail.com>
2025-03-01 21:49:58 -0500

    Delete estado - update Completado

--- task_management/task_app/models.py
+++ task_management/task_app/models.py
@@ -7,16 +7,10 @@ class Task(models.Model):
         (2, 'Media'),
         (3, 'Alta'),
     ]
-    STATUS_CHOICES = [
-        ('Pendiente', 'Pendiente'),
-        ('En progreso', 'En progreso'),
-        ('Completada', 'Completada'),
-    ]
 
     titulo = models.CharField(max_length=255)
     descripcion = models.TextField(blank=True, null=True)
     prioridad = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
-    estado = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pendiente')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_limite = models.DateField(null=True, blank=True)
     completado = models.BooleanField(default=False)
